2912bb94301248d208f9e455fa257a597a72765d
SanmartinJose <132719688+SanmartinJose@users.noreply.github.com>
2023-05-18 11:18:33 -0500

    Workshops V0.1

---
+++ ws/sanMartin/u1/ChickenFarmSimulator/src/es/edu/espe/chickenfarm/control/Poop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.edu.espe.chickenfarm.control;
+
+/**
+ *
+ * @author Sanmertin Jose, OOP-ERATION-GOSLING,DCC-ESPE
+ */
+public class Poop {
+
+    public Poop() {
+    }
+    
+}
