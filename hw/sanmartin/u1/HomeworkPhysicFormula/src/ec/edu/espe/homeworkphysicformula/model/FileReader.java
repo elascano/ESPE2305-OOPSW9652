929429787ffb0c7c7be242500bde94e96e6a1ac9
SanmartinJose <132719688+SanmartinJose@users.noreply.github.com>
2023-05-21 21:36:42 -0500

    hw v1.0

---
+++ hw/sanmartin/u1/HomeworkPhysicFormula/src/ec/edu/espe/homeworkphysicformula/model/FileReader.java
@@ -0,0 +1,33 @@
+
+package ec.edu.espe.homeworkphysicformula.model;
+
+import ec.edu.espe.homeworkphysicformula.model.Components;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sanmertin Jose, OOP-ERATION-GOSLING,DCC-ESPE
+ */
+public class FileReader {
+    public static void fileReader() {
+        ArrayList<Components> component1 = new ArrayList<Components>();
+        Components c;
+        String[] data;
+        try(Scanner scFile = new Scanner(new File("./components.csv"))){
+            while(scFile.hasNextLine()){
+                data=scFile.nextLine().split(",");
+                c = new Components(Float.parseFloat(data[0]),Float.parseFloat(data[1]),Float.parseFloat(data[2]),Double.parseDouble(data[3]));
+                component1.add(c);
+            }
+    }catch(Exception e){
+            System.out.println("Error");}
+        for (Components component : component1){
+            System.out.println(component);
+        } 
+    }
+}
+    
+
+
---
+++ ws/sanMartin/u1/PhysicFormulas/src/physicformulas/PhysicFormulas.java
@@ -0,0 +1,17 @@
+
+package physicformulas;
+
+/**
+ *
+ * @author Sanmertin Jose, OOP-ERATION-GOSLING,DCC-ESPE
+ */
+public class PhysicFormulas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
