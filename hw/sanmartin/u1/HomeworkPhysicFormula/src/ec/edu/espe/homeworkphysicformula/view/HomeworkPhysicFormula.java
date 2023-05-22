ab1957ce0dd744f85eb7c9e302d0368c5d92c6e2
SanmartinJose <132719688+SanmartinJose@users.noreply.github.com>
2023-05-21 21:53:12 -0500

    homework v1.1

--- hw/sanmartin/u1/HomeworkPhysicFormula/src/ec/edu/espe/homeworkphysicformula/view/HomeworkPhysicFormula.java
+++ hw/sanmartin/u1/HomeworkPhysicFormula/src/ec/edu/espe/homeworkphysicformula/view/HomeworkPhysicFormula.java
@@ -6,42 +6,12 @@ package ec.edu.espe.homeworkphysicformula.view;
  */
 
 
-import static ec.edu.espe.homeworkphysicformula.model.FileReader.fileReader;
-import static ec.edu.espe.homeworkphysicformula.model.FileWritter.fileWriter;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import static ec.edu.espe.homeworkphysicformula.control.Menu.menuController;
+
 public class HomeworkPhysicFormula {
 
  
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        boolean exit = false;
-        int option;
-        while(!exit){
-            System.out.println("---Hello, Welcome to your Final Velocity Calculator---- ");
-            System.out.println("1.- Calculate Velocity \n2.-Read Data \n3.-Exit \nOption: ");
-            try{
-            option= keyboard.nextInt();
-            switch(option){
-                case 1:
-                    System.out.println("Calculate Speed");
-                    fileWriter();
-                    break;
-                case 2:
-                    System.out.println("Read Speed");
-                    fileReader();
-                    break;
-                case 3:
-                    exit=true;
-                    break;
-                default:
-                    System.out.println("Invalid option it must be 1,2 or 3");
-                }        
-            }catch(InputMismatchException e){
-                System.out.println("It must be a number");
-                keyboard.next();
-            }
-        }
-        System.out.println("Bye, I see you soon");
-   }
+       menuController();
+}
 }
