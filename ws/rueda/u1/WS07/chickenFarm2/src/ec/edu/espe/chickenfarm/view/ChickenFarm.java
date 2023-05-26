8a2ef504340fcb5e63ce8a62bc3b628b4b950417
juan784 <juanse.rueda1@gmail.com>
2023-05-25 21:10:15 -0500

    Workshops folder

---
+++ ws/rueda/u1/WS07/chickenFarm2/src/ec/edu/espe/chickenfarm/view/ChickenFarm.java
@@ -0,0 +1,30 @@
+package ec.edu.espe.chickenfarm.view;
+
+import ec.edu.espe.chickenfarm.model.Chicken;
+import ec.edu.espe.chickenfarm.model.ChickenFarmer;
+import ec.edu.espe.chickenfarm.model.Egg;
+import ec.edu.espe.chickenfarm.model.Poop;
+
+
+/**
+ *
+ * @author Juan Rueda, The Javas, DCCO-ESPE
+ */
+public class ChickenFarm {
+    public static void main(String [] args){
+        Chicken chicken;
+        Poop poop;
+        Egg egg;
+        ChickenFarmer chickenFarmer;
+        int a;
+        a=0;
+        chicken= new Chicken();
+        String name;
+        name="Juan Rueda";
+            
+        System.out.println("My name is ----->"+ name);
+               
+    }
+        
+}
+   
