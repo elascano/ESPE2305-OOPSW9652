9d7e284ceee70b64822dbab1aff37a83d4a53b08
y6u7i8o9p0Y&U/I(O)P= <alejodiego536@gmail.com>
2023-05-30 11:10:34 -0500

    upload ws 09 and 10

---
+++ ws/casignia/u1/ws10Libraries/SalesApp/src/ec/edu/espe/sales/view/Sales.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.sales.view;
+
+/**
+ *
+ * @author Diego Casignia, Techware, DCCO-ESPE
+ */
+public class Sales {
+    public static void main(String[] args){
+        System.out.println("--- Diego sales system ---");
+        System.out.println("What do you want to buy today?");
+    }
+}
