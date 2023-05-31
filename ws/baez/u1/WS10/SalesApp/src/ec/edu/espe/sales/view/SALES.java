70cc9159de455678f1e0a3e13800ea83728766f7
Báez Jaramillo Gabriel Alejandro <132725537+GabrielBaezJ@users.noreply.github.com>
2023-05-30 23:57:44 -0500

    Workshop V1.0

---
+++ ws/baez/u1/WS10/SalesApp/src/ec/edu/espe/sales/view/SALES.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.sales.view;
+
+/**
+ *
+ * @author Gabriel Baez, Techware, DCCO-ESPE
+ */
+public class SALES {
+    public static void main(String[] args) {
+        System.out.println("-------Gabriel's sales system------");
+        System.out.println("What do you want to buy today");
+    }
+}
