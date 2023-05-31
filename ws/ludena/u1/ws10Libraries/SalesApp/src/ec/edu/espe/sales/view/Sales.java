059356a8a0fc07f4469210069ef0fa2eb76e773c
Edison Ludeña <premusic314@gmail.com>
2023-05-30 22:54:08 -0500

    ws09 - ws10

---
+++ ws/ludena/u1/ws10Libraries/SalesApp/src/ec/edu/espe/sales/view/Sales.java
@@ -0,0 +1,13 @@
+
+package ec.edu.espe.sales.view;
+
+/**
+ *
+ * @author Edison Ludeña, BugBuster, DCCO-ESPE
+ */
+public class Sales {
+    public static void main(String[] args) {
+        System.out.println("----- Edison's sales system ----");
+        System.out.println("What do you want to buy today");
+    }
+}
