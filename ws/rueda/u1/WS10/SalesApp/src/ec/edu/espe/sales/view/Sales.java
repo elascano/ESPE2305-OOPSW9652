de13575415f0992378d5c7deb935e04dc1ef8c85
juan784 <juanse.rueda1@gmail.com>
2023-06-02 23:49:49 -0500

    Workshops folder

---
+++ ws/rueda/u1/WS10/SalesApp/src/ec/edu/espe/sales/view/Sales.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.sales.view;
+
+/**
+ *
+ * @author Juan Rueda, The Javas, DCCO-ESPE
+ */
+public class Sales {
+    public static void main(String[] args) {
+        System.out.println("----Juan Rueda----");
+        System.out.println("What do you want to buy today");
+    }
+}
