5ad9a3b3bd120a4b582cc7ffe082767e81ce4f2e
Báez Jaramillo Gabriel Alejandro <132725537+GabrielBaezJ@users.noreply.github.com>
2023-06-15 00:13:00 -0500

    Workshop V1.0

---
+++ ws/baez/u2/WS15/Code/JavaApplication14/src/ec/edu/espe/techStore/utils/ModifyCellphone.java
@@ -0,0 +1,16 @@
+
+package ec.edu.espe.techStore.utils;
+
+import ec.edu.espe.techStore.model.CellphoneData;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jilmar Calderon, Techware, DCCO-ESPE
+ */
+public class ModifyCellphone {
+    
+    
+}
---
+++ ws/baez/u2/WS15/Code/JavaApplication14/src/ec/edu/espe/techStore/view/Cellphone.java
@@ -0,0 +1,21 @@
+
+package ec.edu.espe.techStore.view;
+
+import ec.edu.espe.techStore.model.CellphoneData;
+import ec.edu.espe.techStore.utils.FileManager;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jilmar Calderon, Techware, DCCO-ESPE
+ */
+public class Cellphone {
+   
+public static void main(String[] args) throws FileNotFoundException {
+        Menu menu = new Menu();
+        menu.showMenu();
+    }
+}
