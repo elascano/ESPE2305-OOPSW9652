be10ba1b08990e624474936e01edd7cbdba04699
Javier Ramos <0998736909ramos@gmail.com>
2023-06-15 01:26:01 -0500

    Add Folder

---
+++ ws/ramos/u2/WS15Libraries/WS15Libraries/ToyStoreSystem/src/ec/edu/espe/view/ToyStoreSystem.java
@@ -0,0 +1,15 @@
+
+package ec.edu.espe.view;
+
+import ec.edu.espe.util.FileManager;
+
+/**
+ *
+ * @author Sanmertin Jose, OOP-ERATION-GOSLING,DCC-ESPE
+ */
+public class ToyStoreSystem {
+    public static void main(String[] args) {
+        FileManager fm = new FileManager();
+        fm.addFigure();
+    }
+}
