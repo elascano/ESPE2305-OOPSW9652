d7d0844b7fbfac52acdfcaa61b72cad9996c8128
ZuritaPablo <pablozurita.jr@outlook.com>
2023-07-05 10:36:06 -0500

    WorkShop22 - GUI

---
+++ ws/zurita/U2/WS22/ContactsSystem/src/com/erationgosling/contactssystem/model/Contact.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.erationgosling.contactssystem.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Labs-DCCO
+ */
+public class Contact {
+    int id;
+    String fullName;
+    String contactName;
+    String phoneNumber;
+    String email;
+    String gender;
+    String type;
+    Date bornOnDate;
+    ArrayList<String> hobbies;
+    String comments;
+    String favoriteTeam;
+    
+           
+}
