8f8b205ac254005cd245599b1f161465069f8ed9
mirandaalison <mirandacreamer@gmail.com>
2023-07-05 20:37:09 -0500

    HW14 added

---
+++ hw/miranda/u2/HW14Gui/Contacts_/src/ec/edu/espe/contacts/model/Contact.java
@@ -0,0 +1,22 @@
+package ec.edu.espe.contacts.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Alison Miranda, Bug Busters, DCCO-ESPE
+ */
+public class Contact {
+    int id;
+    String fullName;
+    String contactName;
+    String phoneNumber;
+    String email;
+    String gender;
+    String type;
+    Date bornOnDate;
+    ArrayList<String> hobbies;
+    String comments;
+    String favoriteTeam;
+}
