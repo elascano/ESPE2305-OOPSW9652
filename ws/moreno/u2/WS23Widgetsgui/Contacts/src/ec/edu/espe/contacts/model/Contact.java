8295ae982dd423e7670aab08e69390651f98223d
PaulM92 <psmoreno@espe.edu.ec>
2023-07-09 14:29:31 -0500

    HW&WS

---
+++ ws/moreno/u2/WS23Widgetsgui/Contacts/src/ec/edu/espe/contacts/model/Contact.java
@@ -0,0 +1,24 @@
+
+package ec.edu.espe.contacts.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Paul Moreno,The Encoders,DCCO-ESPE
+ */
+public class Contact {
+    int id;
+    String fullName;
+    String contactName;
+    String phoneNumber;
+    String email;
+    String gender;
+    String type;
+    Date bornOnDate;
+    ArrayList<String> hobbies;
+    String comments;
+    String favoriteTeam;
+          
+}
