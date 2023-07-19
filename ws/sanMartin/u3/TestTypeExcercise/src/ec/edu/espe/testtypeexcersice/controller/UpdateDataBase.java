c87b797b05970170c8ae6b2b6fe215cf51bea4d7
SanmartinJose <132719688+SanmartinJose@users.noreply.github.com>
2023-07-19 02:25:35 -0500

    WSU3 added

---
+++ ws/sanMartin/u3/TestTypeExcercise/src/ec/edu/espe/testtypeexcersice/controller/UpdateDataBase.java
@@ -0,0 +1,16 @@
+
+package ec.edu.espe.testtypeexcersice.controller;
+
+import ec.edu.espe.testtypeexcersice.utils.MongoDBConection;
+
+/**
+ *
+ * @author Sanmertin Jose, OOP-ERATION-GOSLING,DCC-ESPE
+ */
+public class UpdateDataBase {
+    
+    
+    
+   
+    
+}
