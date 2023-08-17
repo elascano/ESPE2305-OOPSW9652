0da83fceff35c389f3d9e6b392e514430c64666a
ZuritaPablo <pablozurita.jr@outlook.com>
2023-08-17 10:50:02 -0500

    ExamFinal - Im scared

---
+++ exams/u3/Zurita/PracticeExam/src/ec/edu/espe/practiceexam/controller/SortingStrategy.java
@@ -0,0 +1,10 @@
+
+package ec.edu.espe.practiceexam.controller;
+
+/**
+ *
+ * @author Pablo Zurita, OOP-ERATION-GOSLING,DCC-ESPE
+ */
+public interface SortingStrategy {
+    public int[] sort(int data[]);
+}
