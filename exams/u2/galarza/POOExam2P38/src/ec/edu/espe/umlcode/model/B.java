137a399acb957116785e0a25cbf5d1eb2a3881ed
Diver <diver.cesar123@gmail.com>
2023-08-29 09:28:19 -0500

    Exams unit 2 v2.3

---
+++ exams/u2/galarza/POOExam2P38/src/ec/edu/espe/umlcode/model/B.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.umlcode.model;
+
+/**
+ *
+ * @author Cesar Galarza, BugBusters, DCCO-ESPE
+ */
+public class B extends A{
+    H h = new H() {};
+}
---
+++ exams/u2/galarza/POOExam2P38/src/ec/edu/espe/umlcode/model/G.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.umlcode.model;
+
+/**
+ *
+ * @author Cesar Galarza, BugBusters, DCCO-ESPE
+ */
+public class G implements H{
+    private J j(){
+        return null;
+    }
+}
